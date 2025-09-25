37559d2aab5765d687de4a38cada07514e50fb55
Tina Wang <txw@mit.edu>
2025-09-25 12:24:18 -0400

    add cool splash cursor effect, to be updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
 import Navbar from "@/app/navbar"
+import SplashCursor from "@/app/splashcursor";
 
 export default function Home() {
   return (
     <div className="font-sans min-h-screen mx-auto p-8 pb-20 gap-16 sm:p-20">
-      <Navbar />
-      <main className="flex flex-col gap-[32px] row-start-2 sm:items-start align-left">
-        <header className="text-8xl font-title font-bold text-left">TiNA WANG.</header>
-        <section className="font-serif w-lg">
-            <p className="my-5">
-                Hi! I&apos;m Tina, and I&apos;m a computer science major especially interested in systems
-                and solving interesting problems. I love exploring the interactions that arise
-                between humans and their environment. Talk to me about anything CS, philosophy,
-                chinese history, art.
-            </p>
-        </section>
-      </main>
+        <Navbar />
+        <main className="flex flex-col gap-[32px] row-start-2 sm:items-start align-left">
+            <header className="text-8xl font-title font-bold text-left">TiNA WANG.</header>
+            <section className="font-serif w-lg">
+                <p className="my-5">
+                    Hi! I&apos;m Tina, and I&apos;m a computer science major especially interested in systems
+                    and solving interesting problems. I love exploring the interactions that arise
+                    between humans and their environment. Talk to me about anything CS, philosophy,
+                    chinese history, art.
+                </p>
+            </section>
+        </main>
+        <SplashCursor />
     </div>
   );
 }
